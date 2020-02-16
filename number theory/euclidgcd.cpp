f485ab7a4a6a2e15e90979129f2092e82b820a0b
achallion <bhavyarocks19@gmail.com>
2020-02-16 23:38:54 +0530

    gcd by euclid theorem

--- number theory/euclidgcd.cpp
+++ number theory/euclidgcd.cpp
@@ -7,11 +7,16 @@ int gcd(int a, int b)
     return b == 0 ? a : gcd(b, a % b);
 }
 
+int gcdoptimized(int a, int b)
+{
+    return b == 0 ? a : (a % b == 0 ? b : gcd(a % b, b % (a % b)));
+}
+
 int main()
 {
     int a, b;
     cin >> a >> b;
-    cout << "HCF is " << gcd(max(a, b), min(a, b));
+    cout << "HCF is " << gcdoptimized(max(a, b), min(a, b));
     cout << "\nLCM is " << a * b / gcd(a, b);
     return 0;
 }
